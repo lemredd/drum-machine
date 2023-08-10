01793f3ca99cc601677aeee70216242280cfa232
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-10 15:06:31 +0800

    chore: prepare `drum_pads`

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import { useState, type ReactElement } from "react";
 import "./App.css";
 
+const drum_pads = {
+	"Heater 1": {},
+	"Heater 2": {},
+	"Heater 3": {},
+	"Heater 4": {},
+	"Clap": {},
+	"Open-HH": {},
+	"Kick-n'-Hat": {},
+	"Kick": {},
+	"Closed-HH": {}
+};
+
 function App(): ReactElement {
 	return (
 		<>
