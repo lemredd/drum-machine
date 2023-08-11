bdc488ed76b5d836f63ae048188e84ea2bc54d43
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-11 13:16:38 +0800

    chore: give respective `key` props to `drum_pads`

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,42 @@ import { useState, type ReactElement } from "react";
 import "./App.css";
 
 const drum_pads = {
-	"heater-1": {},
-	"heater-2": {},
-	"heater-3": {},
-	"heater-4": {},
-	"clap": {},
-	"open-hh": {},
-	"kick-n'-hat": {},
-	"kick": {},
-	"closed-hh": {}
+	"heater-1": {
+		"key": "q",
+		"audio": "",
+	},
+	"heater-2": {
+		"key": "w",
+		"audio": "",
+	},
+	"heater-3": {
+		"key": "e",
+		"audio": "",
+	},
+	"heater-4": {
+		"key": "a",
+		"audio": "",
+	},
+	"clap": {
+		"key": "s",
+		"audio": "",
+	},
+	"open-hh": {
+		"key": "d",
+		"audio": "",
+	},
+	"kick-n'-hat": {
+		"key": "z",
+		"audio": "",
+	},
+	"kick": {
+		"key": "x",
+		"audio": "",
+	},
+	"closed-hh": {
+		"key": "c",
+		"audio": "",
+	}
 };
 const to_kebab_case = (str: string): string => str.split(/\s|_|(?=[A-Z])/g).join("-").toLocaleLowerCase();
 
