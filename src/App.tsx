94da7db62aa869e4b1268518381f29030ca664a1
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-11 13:26:56 +0800

    revert: unconvert keys to kebab case
    Ref: 394c5b0

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,39 @@ import { useState, type ReactElement } from "react";
 import "./App.css";
 
 const drum_pads = {
-	"heater-1": {
+	"Heater 1": {
 		"key": "q",
 		"audio": "",
 	},
-	"heater-2": {
+	"Heater 2": {
 		"key": "w",
 		"audio": "",
 	},
-	"heater-3": {
+	"Heater 3": {
 		"key": "e",
 		"audio": "",
 	},
-	"heater-4": {
+	"Heater 4": {
 		"key": "a",
 		"audio": "",
 	},
-	"clap": {
+	"Clap": {
 		"key": "s",
 		"audio": "",
 	},
-	"open-hh": {
+	"Open HH": {
 		"key": "d",
 		"audio": "",
 	},
-	"kick-n'-hat": {
+	"Kick n' Hat": {
 		"key": "z",
 		"audio": "",
 	},
-	"kick": {
+	"Kick": {
 		"key": "x",
 		"audio": "",
 	},
-	"closed-hh": {
+	"Closed HH": {
 		"key": "c",
 		"audio": "",
 	}
