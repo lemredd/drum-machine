3faa27c65ac5e2a17589bdff9f821de6b44c3b41
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-11 15:53:15 +0800

    feat: play `drum_pad` audio

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,11 @@ const to_kebab_case = (str: string): string => str.split(/\s|_|(?=[A-Z])/g).join
 function App(): ReactElement {
 	const [audio_to_play, set_audio_to_play] = useState<DrumPadLabel | "">("");
 
+	function handle_click(event: React.MouseEvent<HTMLButtonElement>, label: DrumPadLabel): void {
+		const target = event.target as HTMLButtonElement;
+		const audio = target.firstChild as HTMLAudioElement;
+		audio.play().then(() => set_audio_to_play(label)).catch(err => console.error(err));
+	}
 
 	return (
 		<>
@@ -57,7 +62,11 @@ function App(): ReactElement {
 				</h3>
 				<div className="drum-pads">
 					{drum_pad_labels.map(label => (
-						<button key={label} className="drum-pad" id={to_kebab_case(label)}>
+						<button
+							key={label} className="drum-pad"
+							id={to_kebab_case(label)}
+							onClick={(event): void => handle_click(event, label)}
+						>
 							<audio
 								id={drum_pads[label].hotkey.toLocaleUpperCase()}
 								className="clip"
