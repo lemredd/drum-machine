b14737dd33dd668d1b1180b4cfda0ee48988d7b8
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-12 19:14:10 +0800

    refactor(constant): move to `constants.ts`

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
 import { useState, type ReactElement } from "react";
-import "./App.css";
 
-const drum_pads = {
-	"Heater 1": {
-		"hotkey": "q",
-		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-	},
-	"Heater 2": {
-		"hotkey": "w",
-		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-	},
-	"Heater 3": {
-		"hotkey": "e",
-		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-	},
-	"Heater 4": {
-		"hotkey": "a",
-		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-	},
-	"Clap": {
-		"hotkey": "s",
-		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-	},
-	"Open HH": {
-		"hotkey": "d",
-		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-	},
-	"Kick n' Hat": {
-		"hotkey": "z",
-		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-	},
-	"Kick": {
-		"hotkey": "x",
-		"audio": "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-	},
-	"Closed HH": {
-		"hotkey": "c",
-		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-	}
-} as const;
+import { drum_pads } from "./constants";
+
+import "./App.css";
 
 type DrumPadLabel = keyof typeof drum_pads;
 type DrumPadHotkey = (typeof drum_pads)[DrumPadLabel]["hotkey"]
---
+++ src/constants.ts
@@ -0,0 +1,38 @@
+export const drum_pads = {
+	"Heater 1": {
+		"hotkey": "q",
+		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+	},
+	"Heater 2": {
+		"hotkey": "w",
+		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+	},
+	"Heater 3": {
+		"hotkey": "e",
+		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+	},
+	"Heater 4": {
+		"hotkey": "a",
+		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+	},
+	"Clap": {
+		"hotkey": "s",
+		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+	},
+	"Open HH": {
+		"hotkey": "d",
+		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+	},
+	"Kick n' Hat": {
+		"hotkey": "z",
+		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+	},
+	"Kick": {
+		"hotkey": "x",
+		"audio": "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+	},
+	"Closed HH": {
+		"hotkey": "c",
+		"audio": "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+	}
+} as const;
