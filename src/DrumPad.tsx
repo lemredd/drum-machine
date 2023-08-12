e589cd7cac6624ac67c64add1196e7d9e208bfcc
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-12 20:52:48 +0800

    refactor(component): set `label`
    Is different from the `label` prop of `DrumPad`.
    Also move event listener below `interface Props`.

--- src/DrumPad.tsx
+++ src/DrumPad.tsx
@@ -7,23 +7,24 @@ import { DRUM_PADS, DRUM_PAD_HOTKEYS } from "./constants";
 import to_kebab_case from "./helpers/to_kebab_case";
 import to_title_case from "./helpers/to_title_case";
 
-window.addEventListener("keydown", (event: KeyboardEvent): void => {
-	if (DRUM_PAD_HOTKEYS.indexOf(event.key.toLocaleLowerCase() as DrumPadHotkey) === -1) return;
-
-	const audio = document.querySelector(`audio#${event.key.toUpperCase()}`) as HTMLAudioElement;
-	const drum_pad = audio.parentElement as HTMLButtonElement;
-	const display = document.querySelector("#display") as HTMLHeadingElement;
-
-	audio
-		.play()
-		.then((): void => { display.innerText = to_title_case(drum_pad.id).toLocaleUpperCase(); })
-		.catch((error): void => console.error(error));
-});
-
 interface Props {
 	label: DrumPadLabel
 	set_audio_to_play: React.Dispatch<React.SetStateAction<DrumPadLabel | "">>
 }
+
+window.addEventListener("keydown", (event: KeyboardEvent): void => {
+	if (DRUM_PAD_HOTKEYS.indexOf(event.key.toLocaleLowerCase() as DrumPadHotkey) === -1) return;
+
+	const audio = document.querySelector(`audio#${event.key.toUpperCase()}`) as HTMLAudioElement;
+	const { "id": drum_pad_btn_id } = audio.parentElement as HTMLButtonElement;
+	const label = to_title_case(drum_pad_btn_id).toLocaleUpperCase();
+	const display = document.querySelector("#display") as HTMLHeadingElement;
+
+	audio
+		.play()
+		.then((): void => { display.innerText = label; })
+		.catch((error): void => console.error(error));
+});
 
 export default function DrumPad({ label, set_audio_to_play }: Props): ReactElement {
 	function handle_click(event: React.MouseEvent<HTMLButtonElement>, label: DrumPadLabel): void {
