a822508fd92485bf82c01132cc6e61ace4bdaba2
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-12 21:55:14 +0800

    refactor(component): place inside `useEffect`
    ESLint somehow complains (warns) about this listener function not being
    placed inside the `useEffect`. Will research more into this...

--- src/DrumPad.tsx
+++ src/DrumPad.tsx
@@ -13,31 +13,31 @@ interface Props<TDrumPadLabelState = DrumPadLabel | ""> {
 	set_audio_to_play: React.Dispatch<React.SetStateAction<TDrumPadLabelState>>
 }
 
-export default function DrumPad({ label, audio_to_play, set_audio_to_play }: Props): ReactElement {
+export default function DrumPad({ label, set_audio_to_play }: Props): ReactElement {
 	function handle_click(event: React.MouseEvent<HTMLButtonElement>, label: DrumPadLabel): void {
 		const target = event.target as HTMLButtonElement;
 		const audio = target.firstChild as HTMLAudioElement;
 		audio.play().then(() => set_audio_to_play(label)).catch(err => console.error(err));
 	}
 
-	function listen_for_hotkey_press(event: KeyboardEvent): void {
-		if (DRUM_PAD_HOTKEYS.indexOf(event.key.toLocaleLowerCase() as DrumPadHotkey) === -1) return;
-
-		const audio = document.querySelector(`audio#${event.key.toUpperCase()}`) as HTMLAudioElement;
-		const { "id": drum_pad_btn_id } = audio.parentElement as HTMLButtonElement;
-		const label = to_title_case(drum_pad_btn_id).toLocaleUpperCase();
-
-		audio
-			.play()
-			.then((): void => set_audio_to_play(label as DrumPadLabel))
-			.catch((error): void => console.error(error));
-	}
-
 	useEffect((): () => (void) => {
+		function listen_for_hotkey_press(event: KeyboardEvent): void {
+			if (DRUM_PAD_HOTKEYS.indexOf(event.key.toLocaleLowerCase() as DrumPadHotkey) === -1) return;
+
+			const audio = document.querySelector(`audio#${event.key.toUpperCase()}`) as HTMLAudioElement;
+			const { "id": drum_pad_btn_id } = audio.parentElement as HTMLButtonElement;
+			const label = to_title_case(drum_pad_btn_id).toLocaleUpperCase();
+
+			audio
+				.play()
+				.then((): void => set_audio_to_play(label as DrumPadLabel))
+				.catch((error): void => console.error(error));
+		}
+
 		window.addEventListener("keydown", listen_for_hotkey_press);
 
 		return (): void => window.removeEventListener("keydown", listen_for_hotkey_press);
-	}, [audio_to_play]);
+	}, [set_audio_to_play]);
 
 	return (
 		<button
