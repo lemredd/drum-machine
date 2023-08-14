acedb88495fd58f46c8462ebb7e03f8612a0788b
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-14 20:50:36 +0800

    fix(component): match hotkey properly

--- src/DrumPad.tsx
+++ src/DrumPad.tsx
@@ -15,7 +15,7 @@ interface Props {
 export default function DrumPad(
 	{ label, hotkey_pressed, set_audio_to_play }: Props
 ): ReactElement {
-	const audio_id = DRUM_PADS[label].hotkey.toLocaleUpperCase();
+	const audio_id = DRUM_PADS[label].hotkey;
 	function play(): void {
 		const audio = document.getElementById(audio_id) as HTMLAudioElement;
 		audio.currentTime = 0;
@@ -27,7 +27,7 @@ export default function DrumPad(
 		const is_valid_hotkey = DRUM_PAD_HOTKEYS
 			.indexOf(hotkey_pressed as DrumPadHotkey) !== -1;
 		if (!is_valid_hotkey) return;
-		const is_matching_hotkey = audio_id.toLocaleLowerCase() === hotkey_pressed;
+		const is_matching_hotkey = `Key${audio_id}` === hotkey_pressed;
 
 		if (is_matching_hotkey && is_valid_hotkey) play();
 	});
