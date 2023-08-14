21b45982e38c20631553622f66b286c7c5d7060b
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-14 10:35:46 +0800

    refactor: listen for hotkey press on `App`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState, type ReactElement } from "react";
+import { useEffect, useState, type ReactElement } from "react";
 
 import type { DrumPadHotkey, DrumPadLabel } from "./types";
 
-import { DRUM_PAD_LABELS } from "./constants";
+import { DRUM_PAD_HOTKEYS, DRUM_PAD_LABELS } from "./constants";
 
 import DrumPad from "./DrumPad";
 
@@ -11,6 +11,15 @@ import "./App.css";
 function App(): ReactElement {
 	const [audio_to_play, set_audio_to_play] = useState<DrumPadLabel | "">("");
 	const [hotkey_pressed, set_hotkey_pressed] = useState<DrumPadHotkey | "">("");
+
+	useEffect(() => {
+		window.addEventListener("keydown", (event: KeyboardEvent) => {
+			const key = event.key as DrumPadHotkey;
+			const is_valid_hotkey = DRUM_PAD_HOTKEYS.indexOf(key) !== -1;
+
+			if (is_valid_hotkey) set_hotkey_pressed(key);
+		});
+	});
 
 	return (
 		<div id="drum-machine">
